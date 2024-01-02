88c5e988f74004636cd8347e9a6de4d20cf31e28
Rafael Pavan <rafaelpavan95@gmail.com>
2024-01-02 10:17:39 -0300

    Class 1

--- recursion.py
+++ recursion.py
@@ -4,26 +4,21 @@ import numpy as np
 def fact(n):
 
     if n == 1:
-
         return 1
     
     else:
-
         return n*fact(n-1)
     
 
 def fib(n):
 
     if n == 1:
-
         return 1
     
     elif n == 0: 
-
         return 0
     
     else:
-
         return fib(n-1)+fib(n-2)
     
 
